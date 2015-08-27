f4273fe27119bb72b83b8f10757c64eba4dc0cf2
Braden Marshall <braden1996@hotmail.co.uk>
2015-08-27 18:38:05 +0100

    :tada: Updated gulpfile.js to lint our CoffeeScript and stopped our watch task from breaking after an error occured when compiling our SCSS or CoffeeScript.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
 var concat = require('gulp-concat');
 var del = require('del');
 var gzip = require('gulp-gzip');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
-var mainBowerFiles = require('main-bower-files');
 var minifycss = require('gulp-minify-css');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
+var mainBowerFiles = require('main-bower-files');
+var notifier = require('node-notifier');
 
+
+// General Directories
 var DIR_BOWER = 'bower_components';
-
 var DIR_SRC = 'src/**/static';
 var DIR_DEST = 'static';
-
 var DIR_PROJECT = 'src/braden_marshall';
 var DIR_SRC_PROJECT = DIR_PROJECT + '/assets';
 var DIR_DEST_PROJECT = DIR_PROJECT + '/static';
 
+
+// Task-Specific Directories
 var OPTIONS = {
   CSS: {
     src: DIR_DEST + '/**/css/**/*.css',
@@ -68,24 +72,27 @@ var OPTIONS = {
       level: 9
     }
   },
-
-  DEL: {
-    production: DIR_DEST + '/*'
-  }
 };
 
-// Delete task
-gulp.task('deletebuild', function() {
-  //del(OPTIONS.DEL['build']);
+
+// Safely Log Errors
+function swallowError(error){
+  notifier.notify({
+    'title': 'Gulp: an error has occured!',
+    'message': error.stack
+  });
+  gutil.log(gutil.colors.red('Error'), error);
+  this.end();
+}
+
+
+// Stylesheets (CSS & SCSS)
+gulp.task('scss', function() {
+  return gulp.src(OPTIONS.SCSS['src'])
+    .pipe(sass(OPTIONS.SCSS['config']).on('error', swallowError))
+    .pipe(gulp.dest(OPTIONS.SCSS['dest']));
 });
 
-gulp.task('deleteprod', function() {
-  del(OPTIONS.DEL['production']);
-});
-
-gulp.task('delete', ['deletebuild', 'deleteprod']);
-
-// Compile our CSS
 gulp.task('css', function() {
   return gulp.src(OPTIONS.CSS['src'])
     .pipe(autoprefixer())
@@ -93,56 +100,73 @@ gulp.task('css', function() {
     .pipe(gulp.dest(OPTIONS.CSS['dest']));
 });
 
-// Compile our SCSS
-gulp.task('scss', function() {
-  return gulp.src(OPTIONS.SCSS['src'])
-    .pipe(sass(OPTIONS.SCSS['config']).on('error', gutil.log))
-    .pipe(gulp.dest(OPTIONS.SCSS['dest']));
+
+// CoffeeScript
+gulp.task('coffeelint', function() {
+  return gulp.src(OPTIONS.COFFEE['src'])
+    .pipe(coffeelint())
+    .pipe(coffeelint.reporter('coffeelint-stylish'))
 });
 
-// Compile Bower JavaScript
+gulp.task('coffee', ['coffeelint'], function() {
+  return gulp.src(OPTIONS.COFFEE['src'])
+    .pipe(coffee().on('error', swallowError))
+    .pipe(concat(OPTIONS.COFFEE['filename']))
+    .pipe(gulp.dest(OPTIONS.COFFEE['dest']));
+});
+
+
+// JavaScript
 gulp.task('bowerjs', function() {
   return gulp.src(mainBowerFiles(["**/*.js"]))
     .pipe(concat(OPTIONS.BOWERJS['filename']))
     .pipe(gulp.dest(OPTIONS.BOWERJS['dest']));
 });
 
-// Compile our JavaScript
 gulp.task('js', function() {
   return gulp.src(OPTIONS.JS['src'])
     .pipe(uglify())
     .pipe(gulp.dest(OPTIONS.JS['dest']));
 });
 
-// Compile our CoffeeScript
-gulp.task('coffee', function() {
-  return gulp.src(OPTIONS.COFFEE['src'])
-    .pipe(coffee().on('error', gutil.log))
-    .pipe(concat(OPTIONS.COFFEE['filename']))
-    .pipe(gulp.dest(OPTIONS.COFFEE['dest']));
+
+// Development Tasks
+gulp.task('deletedev', function() {
+  del(OPTIONS.COFFEE['dest'] + "/" + OPTIONS.COFFEE['filename']);
+  del(OPTIONS.SCSS['dest']);
 });
 
-// Execute all our development tasks
-gulp.task('buildapp', ['deletebuild'], function(){
+gulp.task('builddev', ['deletedev'], function(){
   runSequence(
     ['scss', 'coffee', 'bowerjs']
   );
 });
 
-// Execute all our production tasks
+
+// Production Tasks
+gulp.task('deleteprod', function() {
+  del(DIR_DEST + '/*');
+});
+
 gulp.task('buildprod', ['deleteprod'], function(){
   runSequence(
     ['css', 'js']
   );
 });
 
-// Default task
+
+// Default Tasks
 gulp.task('default', function() {
   runSequence(
-    'buildapp',
+    'builddev',
     'buildprod'
   );
 });
+
+
+// Delete Everything!
+gulp.task('delete', ['deletebuild', 'deleteprod']);
+
 
 // Watch Files For Changes
 gulp.task('watch', ['default'], function() {
