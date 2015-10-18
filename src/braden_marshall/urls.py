b7f83092730f410034d1b25d7f0d0351ee0cc090
Braden Marshall <braden1996@hotmail.co.uk>
2015-10-18 18:56:33 +0100

    Removed Django Reset Framework stuff.

--- src/braden_marshall/urls.py
+++ src/braden_marshall/urls.py
@@ -16,29 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from django.contrib import admin
-from rest_framework import routers, serializers, viewsets
 
 from django.views.generic import TemplateView
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
-    url(r'^rest/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^account/', include('registration.backends.default.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r"^account/", include("account.urls")),
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home')
 ]
