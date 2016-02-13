4d61c52dd1e7a8969af5eb712ff40c0c16ff1e67
Braden Marshall <braden1996@hotmail.co.uk>
2016-02-13 16:48:42 +0000

    Replace 'gulp-minify-css' with 'gulp-cssnano'.

--- gulpfile.js/tasks/production/css.js
+++ gulpfile.js/tasks/production/css.js
@@ -1,11 +1,11 @@
 var config       = require("../../config/production/css");
 var gulp         = require("gulp");
 var autoprefixer = require('gulp-autoprefixer');
-var minifyCss    = require("gulp-minify-css");
+var cssnano    = require("gulp-cssnano");
 
 gulp.task("production-css", function() {
 	return gulp.src(config.source)
 		.pipe(autoprefixer())
-		.pipe(minifyCss())
+		.pipe(cssnano())
 		.pipe(gulp.dest(config.destination));
 });
