995fac27b646561f4c487a99d02ee3a582121c0e
Braden Marshall <braden1996@hotmail.co.uk>
2016-02-13 17:25:50 +0000

    Moved project assets (JS and SCSS) into subdirectory 'base'.

--- gulpfile.js/config/index.js
+++ gulpfile.js/config/index.js
@@ -2,8 +2,8 @@ var config = {}
 
 config.source = "./src"
 config.project = config.source + "/braden_marshall"
-config.projectAssets = config.project + "/assets"
-config.projectStatic = config.project + "/static"
+config.projectAssets = config.project + "/assets/base/"
+config.projectStatic = config.project + "/static/base/"
 config.projectCollectStatic = config.source + "/static"
 
 module.exports = config
