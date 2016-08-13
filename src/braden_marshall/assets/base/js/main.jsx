99c8d2dae4d13ee9fa1e0d71a6e63af170a8facb
Braden Marshall <braden1996@hotmail.co.uk>
2016-08-13 11:14:27 +0100

    Extend React demo file

--- src/braden_marshall/assets/base/js/main.jsx
+++ src/braden_marshall/assets/base/js/main.jsx
@@ -8,3 +8,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class CommentBox extends React.Component {
+  render() {
+    return <div className="commentBox">Hello, world! I am a CommentBox.</div>;
+  }
+}
+
+ReactDOM.render(
+  <div className="block block--fill">
+    <section className="row">
+      <CommentBox />
+    </section>
+  </div>,
+  document.getElementById('l-site__content__main')
+);
