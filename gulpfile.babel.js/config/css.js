c9e0e3da888f23f2e03155d4e3d0e56a1a542d32
Braden Marshall <braden1996@hotmail.co.uk>
2016-08-28 23:02:03 +0100

    Modify CSS Gulp tasks to use a custom PostCSS-Sorting order

--- gulpfile.babel.js/config/css.js
+++ gulpfile.babel.js/config/css.js
@@ -6,6 +6,8 @@ import sorting from 'postcss-sorting';
 import stylefmt from 'stylefmt';
 import stylelint from 'stylelint';
 
+import fs from 'fs';
+
 // When I try to destructure this import directly, I just get undefined?
 // However, it seems to work if I perform the destructure post-import.
 // import { assetPath, staticPath } from "./index.js";
@@ -13,15 +15,18 @@ import config from './index.js';
 
 const { assetPath, staticPath } = config;
 
+const sortedConfigFile = join(process.cwd(), '.postcss-sorting');
+const sortingConfigJSON = fs.readFileSync(sortedConfigFile);
+const sortingConfigObj = JSON.parse(sortingConfigJSON);
+
 export default {
   destination: join(staticPath, 'base/css'),
-
   lintDestination: assetPath,
   lintProcessors: [
-    sorting({
+    sorting(Object.assign({
       'empty-lines-before-comment': 1,
       'preserve-empty-lines-between-children-rules': true,
-    }),
+    }, sortingConfigObj)),
     stylefmt(),
     stylelint({
       reporters: [
